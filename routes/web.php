3c0728d426c0739f0089d478ea9f28ec158fd01d
Sanzhar <sanzhar.d3@gmail.com>
2025-04-25 01:25:13 +0500

    update nginx for websocket

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\ChatController;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Auth::routes();
-
-// Redirect /home to /dashboard
-Route::get('/home', function() {
-    return redirect('/dashboard');
-});
-
-// Group routes that require authentication
-Route::middleware(['auth'])->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    Route::get('/chat/{user}', [ChatController::class, 'show'])->name('chat');
-
-    Route::get('/messages/{user}', [ChatController::class, 'getMessages']);
-    Route::post('/messages/{user}', [ChatController::class, 'sendMessage']);
 });
